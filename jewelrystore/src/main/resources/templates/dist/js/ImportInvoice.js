ef48b7a48ff450d9a0920d41bb603db1fd5c6eae
DESKTOP-J0GT7U3\ASUS <nhuttan1325@gmail.com>
2024-06-19 19:43:29 +0700

    delete dít

--- jewelrystore/src/main/resources/templates/dist/js/ImportInvoice.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    const modal = $('#productSelectionModal');
-
-    function toggleModal() {
-        modal.toggleClass('hidden');
-    }
-
-    $('#openProductModal, #closeProductModal, #cancelProductSelection, #submitProductSelection').on('click', toggleModal);
-});
